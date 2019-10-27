b1fa9133d2abb3b044b0d941df261579a8c15c8c
kraft-owaka <owakakraft@gmail.com>
2019-10-27 21:24:58 +0300

    added an onlclick event listener

--- script/script.js
+++ script/script.js
@@ -1,3 +1,4 @@
+
 var mylist ["akosua","audwuoa","abenaa","akua","yaa","afa","ama"]
 
 var YY = 2020;
@@ -12,3 +13,7 @@ var mylist = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
 var mylist = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afaa","Ama"];
 
 var myday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday"];
+
+var a = document.getElementById("btnClick");
+// creates a button "a" from the "btnClick" in html
+a.addEventListener("click", function(){// add an onClick event litener to btClick so that when clicked, it runs the function})
