6d75107998c7d590a9050e942ef282d40c8d5093
kraft-owaka <owakakraft@gmail.com>
2019-10-27 21:08:49 +0300

    added a fielset

--- script/script.js
+++ script/script.js
@@ -1,3 +1,14 @@
 var mylist ["akosua","audwuoa","abenaa","akua","yaa","afa","ama"]
 
-var
+var YY = 2020;
+var MM = 11;
+var DD = 04;
+var CC = (YY/100).toFixed();
+parseInt(CC);
+
+
+var mylist = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+
+var mylist = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afaa","Ama"];
+
+var myday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday"];
